278f72335d62d1274cb117871c8ff0024934898f
eriksamjr2 <eriksamjr2@gmail.com>
2026-01-27 09:10:29 +0100

    lagt til java på logginnside, fungerer ikke sånn det er nå

--- logginnside.js
+++ logginnside.js
@@ -3,7 +3,7 @@ import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 /** 1) Koble til Supabase (BYTT til dine nøkler) */
 const supabase = createClient(
   "https://xkjtwmewoqcqmbievuoc.supabase.co", // ← Project URL
-  "sb_publishable_-iSCpQNQiil5AEcbVrAaSg_raue1Rg5" // ← anon public key
+  "sb_publishable_-iSCpQNQiil5AEcbVrAaSg_raue1Rg5", // ← anon public key
 );
 
 /** 2) Finn elementer */
